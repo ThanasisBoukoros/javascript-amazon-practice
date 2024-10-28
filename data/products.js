575577fbb115c4563cea11cae58c0d9d572c729b
ThanasisBoukoros <tehnusisb@gmail.com>
2024-10-28 06:42:45 +0200

    learn more details about classes and this

--- data/products.js
+++ data/products.js
@@ -59,10 +59,38 @@ class Clothing extends Product {
     <a href="${this.sizeChartLink}" target="_blank">Size chart</a>
    `;   
   }
+  
+}
+/*
+const date = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString());
 
+
+console.log(this);
+
+const object2 = {
+  a: 2,
+  b: this.a
 }
 
 
+function logThis() {
+  console.log(this);
+}
+
+
+this
+logThis();
+logThis.call('hello');
+
+const object3 = {
+  method:()=>{
+    console.log(this);
+  }
+};
+object3.method();
+*/
 
 export const products = [
   {
